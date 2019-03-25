a23ccbb21d175ee840143183a2ec1624785848b8
marcelo <marcelolayera@gmail.com>
2019-03-25 12:24:35 -0700

    SVG, Pause

--- hw_06/scripts/main.js
+++ hw_06/scripts/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
-        $('.modal-popper').click(function () {
+    $('.carousel').carousel({
+        interval: 500,
+    });
+
+    $('.carousel-pauser').click(function () {
+        $('.carousel').carousel('pause');
+    });
+
+    $('.carousel').on('slid.bs.carousel', function (event) {
+        console.log(event);
+    });
+
+    $('.modal-popper').click(function () {
         $('.modal').modal({ backdrop: 'static' });
     });
+
 });
