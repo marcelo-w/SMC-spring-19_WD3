c252a7eddc395e6091e163ee9262ad2169c1dbc8
marcelo <marcelolayera@gmail.com>
2019-03-31 17:56:52 -0700

    Jumbotron
     Low Poly BKGD

---
+++ class_07_midterm/scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 1500,
+    });
+
+    $('.carousel-pauser').click(function () {
+        $('.carousel').carousel('pause');
+    });
+
+    $('.carousel').on('slid.bs.carousel', function (event) {
+        console.log(event);
+    });
+
+    $('.modal-popper').click(function () {
+        $('.modal').modal({ backdrop: 'static' });
+    });
+
+});
